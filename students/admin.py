ae1fc2dc4e7d368045ae2adc5dc13edbf4eb76a1
Medasani Chaithanya <61534480+medasanichaithanya@users.noreply.github.com>
2023-10-26 21:25:37 +0530

    migrations

--- students/admin.py
+++ students/admin.py
@@ -3,11 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import StudentDetails,SubjectMarks
 
 class StudentDetailsAdmin(UserAdmin):
-    # Define the list of fields to be displayed in the admin list view
     list_display = ('username', 'first_name', 'last_name', 'is_staff')
 
-    # Optionally, you can customize other admin options, such as fieldsets
-
-# Register the custom admin class for your custom user model
+ 
 admin.site.register(StudentDetails, StudentDetailsAdmin)
 admin.site.register(SubjectMarks)
